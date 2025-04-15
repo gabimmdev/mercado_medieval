e6523721ffd0c0417cb96c03e73de81ba2f9eb4b
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-15 12:23:28 -0300

    feat: troca de item DTO

---
+++ src/main/java/com/mercado/medieval/dto/TrocaItemDTO.java
@@ -0,0 +1,42 @@
+package com.mercado.medieval.dto;
+
+public class TrocaItemDTO {
+    private Long idPersonagem1;
+    private Long idItem1;
+    private Long idPersonagem2;
+    private Long idItem2;
+
+    // getters e setters
+    public Long getIdPersonagem1() {
+        return idPersonagem1;
+    }
+
+    public void setIdPersonagem1(Long idPersonagem1) {
+        this.idPersonagem1 = idPersonagem1;
+    }
+
+    public Long getIdItem1() {
+        return idItem1;
+    }
+
+    public void setIdItem1(Long idItem1) {
+        this.idItem1 = idItem1;
+    }
+
+    public Long getIdPersonagem2() {
+        return idPersonagem2;
+    }
+
+    public void setIdPersonagem2(Long idPersonagem2) {
+        this.idPersonagem2 = idPersonagem2;
+    }
+
+    public Long getIdItem2() {
+        return idItem2;
+    }
+
+    public void setIdItem2(Long idItem2) {
+        this.idItem2 = idItem2;
+    }
+}
+
