1a7571a6202a5bf254d755f63e614b02b97730d0
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-15 11:24:07 -0300

    first commit

---
+++ src/main/java/com/mercado/medieval/mercado_medieval/MercadoMedievalApplication.java
@@ -0,0 +1,13 @@
+package com.mercado.medieval.mercado_medieval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoMedievalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoMedievalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercado/medieval/mercado_medieval/MercadoMedievalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercado.medieval.mercado_medieval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoMedievalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
